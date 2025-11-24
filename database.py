cb465c55b67368f6354912c2546e3227856d9877
Samriddhi Singh <samriddhisingh928@gmail.com>
2025-11-25 02:08:50 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,14 +4,14 @@ def create_db():
     conn = sqlite3.connect("expenses.db")
     cursor = conn.cursor()
 
-    cursor.execute("""
+    cursor.execute('''
         CREATE TABLE IF NOT EXISTS expenses (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             amount REAL NOT NULL,
             category TEXT NOT NULL,
             date TEXT NOT NULL
         )
-    """)
+    ''')
     conn.commit()
     conn.close()
 
